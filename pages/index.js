81c80efeda6af7ddba8d45793c0fbd0221067769
T-N9 <thethoriumtension@gmail.com>
2023-10-27 12:32:19 +0630

    :lipstick: Tailwind MUI added
    testing this library

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
+import { Button } from "@material-tailwind/react";
+
 export default function Home() {
   return (
    <main>
-    Hello Flash Kanji.
+    <h1 className="text-2xl text-blue-gray-600">Hello Flash Kanji.</h1>
+    <Button>Click Me</Button>
    </main>
   )
 }
