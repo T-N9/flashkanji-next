982a198b960f8601277354fdfa21bb8bf602703a
T-N9 <thethoriumtension@gmail.com>
2023-10-27 13:47:45 +0630

    :lipstick: NavBar added

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import "@/styles/globals.css";
 import { ThemeProvider } from "@material-tailwind/react";
+import { NavigationBar } from "@/components/common/NavigationBar";
 
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider>
-      <Component {...pageProps} />
+      <main className="container mx-auto">
+        <NavigationBar />
+        <Component {...pageProps} />
+      </main>
     </ThemeProvider>
   );
 }
