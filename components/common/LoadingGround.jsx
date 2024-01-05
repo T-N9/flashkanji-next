68d92acdc525972fd72720605b7df03d589f189c
T-N9 <thethoriumtension@gmail.com>
2024-01-05 12:13:47 +0630

    :lipstick: Loading card spinner updated

--- components/common/LoadingGround.jsx
+++ components/common/LoadingGround.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { ImSpinner3 } from "react-icons/im";
 
-export const LoadingGround = ({mode}) => {
+export const LoadingGround = ({ mode }) => {
   /* mode 1 = flash ground
      mode 2 = jukugo ground
   */
@@ -14,9 +14,9 @@ export const LoadingGround = ({mode}) => {
               return (
                 <div
                   key={index}
-                  className={`bg-orange-50 relative font-primary-serif text-black p-5 rounded-md card min-w-[150px] lg:min-w-[200px] flex justify-center items-center shadow-md `}
+                  className={`bg-orange-50 relative font-primary-serif text-dark p-5 rounded-md card min-w-[150px] lg:min-w-[200px] flex justify-center items-center shadow-md `}
                 >
-                  <ImSpinner3 size={20} className="animate-spin" />
+                  <ImSpinner3 size={40} className="animate-spin" />
                 </div>
               );
             }
@@ -30,9 +30,9 @@ export const LoadingGround = ({mode}) => {
               return (
                 <div
                   key={index}
-                  className={`bg-orange-50 relative font-primary-serif text-black p-5 rounded-md card min-w-[150px] lg:min-w-[200px] flex justify-center items-center shadow-md `}
+                  className={`bg-orange-50 relative font-primary-serif p-5 text-dark rounded-md card min-w-[150px] lg:min-w-[200px] flex justify-center items-center shadow-md `}
                 >
-                  <ImSpinner3 size={20} className="animate-spin" />
+                  <ImSpinner3 size={40} className="animate-spin " />
                 </div>
               );
             }
