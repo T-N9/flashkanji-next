6ee53ac0cfacd8056a06915566e5f497bdce645f
T-N9 <thethoriumtension@gmail.com>
2024-01-06 20:21:14 +0630

    :lipstick: Loading grid fixed

--- components/common/LoadingGround.jsx
+++ components/common/LoadingGround.jsx
@@ -8,7 +8,7 @@ export const LoadingGround = ({ mode }) => {
   return (
     <>
       {mode === 1 && (
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
+        <div className="grid w-full grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
           {Array.from({ length: 10 }, (_, index) => index + 1).map(
             (item, index) => {
               return (
